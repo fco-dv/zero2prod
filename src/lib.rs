7d2dbf22fdd4218de2c248df30282440314e418e
francois.cokelaer <francois.cokelaer@airliquide.com>
2024-05-29 20:30:29 +0200

    integration test ok / add db script and first migration

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,28 @@
 use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
+
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    name: String,
+    email: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(tcp_listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server: Server =
-        HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-            .listen(tcp_listener)?
-            .run();
+    let server: Server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(tcp_listener)?
+    .run();
 
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -23,6 +23,50 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let address = spawn_app();
+    let client: reqwest::Client = reqwest::Client::new();
+    let form_data = "name=francois&email=francois@gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(form_data)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert!(response.status().is_success());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_for_non_valid_form_data() {
+    let address = spawn_app();
+    let client: reqwest::Client = reqwest::Client::new();
+    let test_parameters = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_parameters {
+        let response = client
+            .post(&format!("{}/subscriptions", &address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        )
+    }
+}
+
 fn spawn_app() -> String {
     let tcp_listener = TcpListener::bind("127.0.0.1:0").expect("Fail to create listener");
     let port = tcp_listener.local_addr().unwrap().port();
